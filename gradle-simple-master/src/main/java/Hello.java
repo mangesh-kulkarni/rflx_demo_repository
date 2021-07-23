c5c82c8718cf4081d6a130087082ec79d8054668
mangesh-kulkarni <mangesh.kulkarni@zebra.com>
2021-07-23 12:28:12 +0530

    Updated branch code in main

--- gradle-simple-master/src/main/java/Hello.java
+++ gradle-simple-master/src/main/java/Hello.java
@@ -1,7 +1,7 @@
-
+//My test code MK
 public class Hello {
   
-  public static String GREETING = "Hello world!";
+  public static String GREETING = "Hello Reflexis!";
   
   public static void main(String []args) {
     
